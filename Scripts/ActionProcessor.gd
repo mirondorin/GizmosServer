d99d406273e268001297e221cff52bf904ae9c25
Dorin <mirondorin99@gmail.com>
2021-12-02 23:53:27 +0200

    Fixed action usage
    Added function that uses first available action

--- Scripts/ActionProcessor.gd
+++ Scripts/ActionProcessor.gd
@@ -10,6 +10,24 @@ func attempt_action(_player_container, _info):
 	pass
 
 
+
+# Returns true if player can make an action
+func available_action(player_container) -> bool:
+	var free_action_count = player_container.get_free_action_count(action_id)
+	if not player_container.used_action or free_action_count > 0:
+		return true
+	return false
+
+
+# Uses normal action first if available, else decrementes free action
+func use_first_avaiable_action(player_container) -> void:
+	var used_action = use_action(player_container)
+	if used_action:
+		return
+	used_action = use_free_action(player_container)
+
+
+# Sets player action as used
 func use_action(player_container) -> bool:
 	if not player_container.used_action:
 		player_container.set_used_action(true)
@@ -17,6 +35,7 @@ func use_action(player_container) -> bool:
 	return false
 
 
+# Decrements free action with action id from player
 func use_free_action(player_container) -> bool:
 	var free_action_count = player_container.get_free_action_count(action_id)
 	if free_action_count > 0:
--- Scripts/PickProcessor.gd
+++ Scripts/PickProcessor.gd
@@ -3,7 +3,8 @@ extends ActionProcessor
 
 func attempt_action(player_container, energy_type: int) -> bool:
 	# Should not use action, should only check if player has them until action is successful
-	if use_action(player_container) or use_free_action(player_container):
+	if available_action(player_container):
+		use_first_avaiable_action(player_container)
 		if player_container.get_energy_space() > 0 and EnergyManager.get_row_type_amount(energy_type) > 0:
 			EnergyManager.give_player_energy(player_container, energy_type)
 			return true
